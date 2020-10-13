5b449de594a5f6495ca77ea3fe89c0dcaed4bf2e
Marshalla Robinson <marshallarbnsn@gmail.com>
2020-10-13 10:15:15 -0500

    added row.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone React JS</h1>
-      <Row title= "Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title= "Trending Now" fetchUrl={requests.fetchTrendingNow}/>
-      {/*} <Row title= "Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
+      {/**<Row title= "Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title= "Action Movies" fetchUrl={requests.fetchActionMovies} />
   <Row title= "Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> */}
-     
     </div>
   );
 }
